782998f4c0376688510025e7f648a369dd742976
Khushboo Kaur <ms.khushboo13@gmail.com>
2017-03-01 00:49:15 +0530

    Android Native App Webview Component - WebRTC Compatible

---
+++ app/src/main/java/testapp/tokbox/com/webviewtest/MainActivity.java
@@ -0,0 +1,36 @@
+package testapp.tokbox.com.webviewtest;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://tokbox-basic-app.herokuapp.com");
+        myWebView.setWebChromeClient(new WebChromeClient(){
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                request.grant(request.getResources());
+            }
+        });
+
+
+
+    }
+
+
+}
